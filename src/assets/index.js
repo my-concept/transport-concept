1cbe99b65ea047c29c8019a54dd96763c9af42c6
MariaMP2501 <103878102+MariaMP2501@users.noreply.github.com>
2023-10-31 11:36:23 +0100

    Update index.js
    import icons

---
+++ src/assets/index.js
@@ -0,0 +1,30 @@
+import comfort from "./comfortCar.png"
+import taxi from "./taxiCar.png"
+import van from "./vanCar.png"
+import vip from "./vipCar.png"
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import PersonIcon from '@mui/icons-material/Person'; 
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone'; 
+import CircleIcon from '@mui/icons-material/Circle';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import EuroIcon from '@mui/icons-material/Euro';
+import SearchIcon from '@mui/icons-material/Search';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+
+export{
+    comfort,
+    taxi,
+    van,
+    vip,
+    AddCircleIcon,
+    PersonIcon,
+    LocalPhoneIcon,
+    CircleIcon,
+    AccessTimeIcon,
+    CalendarTodayIcon,
+    EuroIcon,
+    SearchIcon,
+    faEnvelope,
+}
