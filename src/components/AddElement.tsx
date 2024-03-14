7794c6a6f24aa0776379f916768811adfd04833a
badel <a.mandel06@gmail.com>
2024-03-14 16:47:35 +0100

    add addElement

--- src/components/AddElement.tsx
+++ src/components/AddElement.tsx
@@ -1,12 +1,18 @@
 import { Box, Paper, Typography } from "@mui/material";
+import { useState } from "react";
 
 interface AddElementPropsType {
   title: string;
   action?: () => void;
 }
 
-const action = () => console.log("add");
 export const AddElement = ({ title }: AddElementPropsType) => {
+  const [image, setImage] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setImage(e.target.value);
+  };
+  console.log(image);
   return (
     <Box
       sx={{
@@ -14,23 +20,30 @@ export const AddElement = ({ title }: AddElementPropsType) => {
         justifyContent: "center",
         flexDirection: "column",
         alignItems: "center",
-        ":hover": { cursor: "pointer" },
       }}
-      onClick={action}
     >
+      <input
+        onChange={handleChange}
+        id="gogo"
+        type="file"
+        style={{ visibility: "hidden" }}
+      />
       <Typography variant="h5">{title}</Typography>
-      <Paper
-        sx={{
-          width: "6em",
-          height: "6em",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          margin: "0.8em",
-        }}
-      >
-        <Typography variant="h4">+</Typography>
-      </Paper>
+      <label htmlFor="gogo">
+        <Paper
+          sx={{
+            width: "6em",
+            height: "6em",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            margin: "0.8em",
+            ":hover": { cursor: "pointer" },
+          }}
+        >
+          <Typography variant="h4">+</Typography>
+        </Paper>
+      </label>
     </Box>
   );
 };
